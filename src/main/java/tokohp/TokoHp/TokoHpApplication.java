3ad3d11dae7df9125b09e88a90aff293a50947ca
Alekuyz <axelagia@gmail.com>
2022-10-18 19:27:14 +0700

    1

---
+++ src/main/java/tokohp/TokoHp/TokoHpApplication.java
@@ -0,0 +1,13 @@
+package tokohp.TokoHp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoHpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoHpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tokohp/TokoHp/TokoHpApplicationTests.java
@@ -0,0 +1,13 @@
+package tokohp.TokoHp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoHpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
