2ebf33eea6f29b7e73f91a6bf3f6b1c02736afd9
Alekuyz <axelagia@gmail.com>
2022-10-18 19:40:46 +0700

    5

--- src/main/java/tokohp/TokoHp/BarangJpaController.java
+++ src/main/java/tokohp/TokoHp/BarangJpaController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import tokohp.TokoHp.exceptions.IllegalOrphanException;
 import tokohp.TokoHp.exceptions.NonexistentEntityException;
 import tokohp.TokoHp.exceptions.PreexistingEntityException;
@@ -27,7 +28,7 @@ public class BarangJpaController implements Serializable {
     public BarangJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("tokohp_TokoHp_jar_0.0.1-SNAPSHOTPU");
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
